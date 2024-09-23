95aa08a5d8e1c684f2f4fd0d0e611dd45228e041
marinaRimasha <rimasa.marina@gmail.com>
2024-09-23 22:31:29 +0300

    homework 5.2.1/ added project id and script

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "2kn4bo",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
